1394c42dc38c30d34ac84cdd8acc561774c5931a
JanChoro <86385473+JanChoro@users.noreply.github.com>
2023-02-20 09:06:35 -0500

    Second commit siu

--- practice flutter.dart
+++ practice flutter.dart
@@ -1,8 +1,7 @@
-/*MAIN.DART*/
-
 import 'package:flutter/material.dart';
-
-import 'homePage.dart';
+// import 'dart:ui';
+// import 'package:flutter/widgets.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,67 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo D.J',
-      // 
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        
+        primarySwatch: Colors.yellow,
       ),
-
-      home: const HomePage(title:"Home Pge"),
+      home: const HomePage(),
     );
   }
 }
-
-//------------------------------------------------------------------------------
-
-/*homePage.dart*/
-import 'package:flutter/material.dart';
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key, required String title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("App d.j"),
-        // actions: const [
-        //   IconButton(
-        //     onPressed: () {},
-        //    icon: const Icon(Icons.access_alarm),
-        //    color: Colors.red,
-        //     ),
-
-        // ],
-      ),
-      drawer: Drawer(), //Tres barras arriba a la izquierda
-      body:
-      
-          Column(mainAxisAlignment: MainAxisAlignment.center, children: const [
-        // colum( Childre[] )  nos permite colocar varios widgets (texto ene ste caso) al mismo tiempo.
-        
-          
-        Text("Hola",
-            style: TextStyle(
-                color: Colors.blue,
-                fontSize: 30,
-                fontWeight: FontWeight.bold)), //Propiedades del texto
-                
-
-        Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            "como estás hoy",
-            style: TextStyle(
-              color: Colors.red, fontSize: 25, fontWeight: FontWeight.w100),
-          ),
-        ) 
-        
-      ]),
-
-      
-    );
-  }
-}
-//NOTA:
-// CREAR carpeta "assets". En ella, van videos, imágenes, íconos, fuentes, y todo tipo de multimedia
